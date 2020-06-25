9e0eb2bf28a483e849699ae0073a320f388e5a16
vinicius <vinisilva2001@hotmail.com>
2020-06-25 18:33:45 -0300

    Feito!

--- src/devdojo/lacosrepeticao/Aula_02.java
+++ src/devdojo/lacosrepeticao/Aula_02.java
@@ -1,9 +1,24 @@
 package devdojo.lacosrepeticao;
 
+import java.util.Scanner;
+
 public class Aula_02 {
 
 	public static void main(String[] args) {
-	
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um numero");
+		int numero = sc.nextInt();
+		int i = 0;
+		while (i <= numero) {
+			if (i % 2 != 0) {
+				System.out.println("i + " + i);
+
+			}
+			i++;
+		}
+
+		sc.close();
 
 	}
 
