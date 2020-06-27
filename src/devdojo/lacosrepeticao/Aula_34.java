3c4cb7518845eec5dd21e81a3a995e301cc3b54d
vinicius <vinisilva2001@hotmail.com>
2020-06-27 18:00:27 -0300

    first exercise

--- src/devdojo/lacosrepeticao/Aula_34.java
+++ src/devdojo/lacosrepeticao/Aula_34.java
@@ -2,7 +2,11 @@ package devdojo.lacosrepeticao;
 
 public class Aula_34{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //ex01 (terão 2 exercicios nessa aula.
+		for (int i = 15; i <= 200; i++) {
+			System.out.println(i * i);
+			
+		}
 		
 	}
 
